684b418b93a33286936d55a95ded27297d7f83a1
Jason <mickeu420@gmail.com>
2019-01-09 14:02:12 -0500

    duiafdjkafljlkfsl

--- app.py
+++ app.py
@@ -6,3 +6,11 @@ from flask import Flask, render_template, request,session,url_for,redirect,flash
 import util
 
 app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
