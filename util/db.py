9ffa3278deceb923760f32167024d9d7788c286d
Jack Lu <jlu9@stuy.edu>
2019-01-09 13:59:12 -0500

    added db process file

---
+++ util/db.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+# CREATE TABLE users( username TEXT primary key, pw text);
+# CREATE TABLE quiz( id integer primary key, name text, owner text);
+# CREATE TABLE content( term text, definition text);
+# CREATE TABLE folders( owner text, name text, id integer primary key, quizzes text);
+
+def login(user, pw):
+    '''Verifies username matches with pw'''
+    printf("!!!!!!!!11")
+
+def register(user, pw):
+    '''Add user to db'''
+    printf("???????")
