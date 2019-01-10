164abc77e855873b004f42c588f5d8b039cdb4fa
Jack Lu <jlu2025@gmail.com>
2019-01-10 00:05:10 -0500

    login and register functions

--- util/db.py
+++ util/db.py
@@ -5,10 +5,46 @@ import sqlite3
 # CREATE TABLE content( term text, definition text);
 # CREATE TABLE folders( owner text, name text, id integer primary key, quizzes text);
 
-def login(user, pw):
+
+def login(user_check, pw_check):
     '''Verifies username matches with pw'''
-    printf("!!!!!!!!11")
+    db = sqlite3.connect('../data/database.db')
+    cursor = db.cursor()
+
+    cmd = "SELECT pw FROM users WHERE users.username = ?"
+    params = (user_check,)
+    cursor.execute(cmd, params)
+    check = cursor.fetchone()
+
+    db.commit()
+    db.close()
+
+    if check == None:
+        # username doesnt exist
+        print("usrname dont exist")
+        return False
+    return check[0] == pw_check
+
+    
+
 
 def register(user, pw):
     '''Add user to db'''
-    printf("???????")
+    db = sqlite3.connect('../data/database.db')
+    cursor = db.cursor()
+
+    cmd = "INSERT INTO users VALUES(?, ?)"
+    params = (user, pw)
+    cursor.execute(cmd, params)
+    cmd = "SELECT * FROM users"
+
+    #a = cursor.execute(cmd);
+    #for i in a:
+    #    print(i)
+
+    db.commit()
+    db.close()
+
+
+#register('c','c')
+#print(login('test', '123'))
